7b1f7639c72de4452fef01e8013982be848335ca
aaaaaaa <yeanleyton6@gmail.com>
2024-06-11 13:47:03 -0400

    modified:   ProyectoCumbres/__pycache__/__init__.cpython-312.pyc 	modified:   ProyectoCumbres/__pycache__/settings.cpython-312.pyc 	modified:   ProyectoCumbres/__pycache__/urls.cpython-312.pyc 	modified:   ProyectoCumbres/__pycache__/wsgi.cpython-312.pyc 	modified:   SitioWeb/__pycache__/__init__.cpython-312.pyc 	modified:   SitioWeb/__pycache__/admin.cpython-312.pyc 	modified:   SitioWeb/__pycache__/apps.cpython-312.pyc 	modified:   SitioWeb/__pycache__/models.cpython-312.pyc 	modified:   SitioWeb/__pycache__/urls.cpython-312.pyc 	modified:   SitioWeb/__pycache__/views.cpython-312.pyc 	modified:   SitioWeb/migrations/__pycache__/__init__.cpython-312.pyc 	modified:   SitioWeb/models.py 	modified:   db.sqlite3

--- SitioWeb/models.py
+++ SitioWeb/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+class consulta(models.Model):
+    id_consulta = models.AutoField(db_column='idConsulta', primary_key=True)
+    nombre_c = models.Charfield(max_length = 20)
+    apellido_c = models.Charfield(max_length = 20)
+    numero_c = models.IntegerField()
+    correo_c = models.EmailField(max_length = 254)
+    consulta_c = models.TextField()
+
+class utp (models.Model):
+    nombre_utp = models.CharField(max_length = 20)
+    apellido_utp = models.CharField(max_length = 20)
+    correo_utp = models.EmailField(max_length = 254)
+    fec_contra_utp = models.DateField()
+
+class Video(models.Model):
+    titulo_vid = models.CharField(max_length=100)
+    descripcion_vid = models.TextField()
+
+    def __str__(self):
+        return self.titulo_vid
+
+
+class Imagen(models.Model):
+    titulo_img = models.CharField(max_length=70)
+    descripcion_img = models.TextField()
+
+    def __str__(self):
+        return self.titulo_img
+    
+class Profesor(models.Model):
+    nombre_prof = models.CharField(max_length=20)
+    apellido_prof = models.CharField(max_length=20)
+    correo_prof = models.CharField(max_length=254)
+    fec_contra_prof = models.DateField()
+
+
+class Asistente(models.Model):
+    nombre_asis = models.CharField(max_length=20)
+    apellido_asis = models.CharField(max_length=20)
+    correo_asis = models.CharField(max_length=254)
+    fec_contra_asis = models.DateField()
