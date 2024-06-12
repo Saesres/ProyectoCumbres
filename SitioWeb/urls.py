9500408048dc8dae5843fd993d2330bf45179635
aywey <yeanleyton6@gmail.com>
2024-06-12 14:54:04 -0400

    modified:   ProyectoCumbres/static/CSS/style.css 	modified:   SitioWeb/__pycache__/urls.cpython-312.pyc 	modified:   SitioWeb/__pycache__/views.cpython-312.pyc 	modified:   SitioWeb/templates/paginas/index.html 	modified:   SitioWeb/urls.py 	modified:   SitioWeb/views.py

--- SitioWeb/urls.py
+++ SitioWeb/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, Contacto
 
 urlpatterns = [
     path('index', index, name='index'),
+    path('Contacto', Contacto, name='Contacto'),
 ]
--- SitioWeb/views.py
+++ SitioWeb/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     context={}
     return render(request,'paginas/index.html',context)
+
+def Contacto(request):
+    context={}
+    return render(request,'paginas/Contacto.html', context)
