7dc634b5f9f1d24fef2e3cdca40470bf991f664f
aywey <yeanleyton6@gmail.com>
2024-06-12 13:56:12 -0400

    modified:   ProyectoCumbres/__pycache__/__init__.cpython-312.pyc 	modified:   ProyectoCumbres/__pycache__/settings.cpython-312.pyc 	modified:   ProyectoCumbres/__pycache__/urls.cpython-312.pyc 	modified:   ProyectoCumbres/__pycache__/wsgi.cpython-312.pyc 	modified:   SitioWeb/__pycache__/__init__.cpython-312.pyc 	modified:   SitioWeb/__pycache__/admin.cpython-312.pyc 	modified:   SitioWeb/__pycache__/apps.cpython-312.pyc 	modified:   SitioWeb/__pycache__/models.cpython-312.pyc 	modified:   SitioWeb/__pycache__/urls.cpython-312.pyc 	modified:   SitioWeb/__pycache__/views.cpython-312.pyc 	modified:   SitioWeb/migrations/__pycache__/__init__.cpython-312.pyc 	modified:   SitioWeb/models.py 	modified:   db.sqlite3

--- SitioWeb/models.py
+++ SitioWeb/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class consulta(models.Model):
     id_consulta = models.AutoField(db_column='idConsulta', primary_key=True)
-    nombre_c = models.Charfield(max_length = 20)
-    apellido_c = models.Charfield(max_length = 20)
+    nombre_c = models.CharField(max_length = 20)
+    apellido_c = models.CharField(max_length = 20)
     numero_c = models.IntegerField()
     correo_c = models.EmailField(max_length = 254)
     consulta_c = models.TextField()
