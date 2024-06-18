78eb71314abe07f322113edccc55349ece2ec3dc
Saesres <ignacioponce2k1@outlook.com>
2024-06-18 19:24:46 -0400

    modified:   ProyectoCumbres/__pycache__/urls.cpython-312.pyc 	modified:   SitioWeb/__pycache__/views.cpython-312.pyc 	new file:   SitioWeb/templates/paginas/Modals.html 	modified:   SitioWeb/views.py

--- SitioWeb/views.py
+++ SitioWeb/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView
 
-
+class CustomLoginView(LoginView):
+    template_name = 'templates/Modals.html'
+    
+    
 # Create your views here.
 def index(request):
     context={}
@@ -11,27 +15,3 @@ def index(request):
 def Contacto(request):
     context={}
     return render(request,'paginas/Contacto.html', context)
-
-
-
-
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            # Redirigir a una página de éxito.
-            return redirect('nombre_de_tu_url_después_del_login')
-        else:
-            # Retornar un mensaje de error de 'login' inválido.
-            return render(request, 'tu_template_de_login.html', {'error': 'Usuario o contraseña inválidos.'})
-    else:
-        return render(request, 'tu_template_de_login.html')
-
-
-
-return render(request, 'tu_template_de_login.html', {'login_success': True})
-
-return render(request, 'tu_template_de_login.html', {'login_success': False, 'error_message': 'Usuario o contraseña inválidos.'})
