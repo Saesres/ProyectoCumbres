f85d220feefce087150b4f860d99a5d4983f8e5c
aywey <yeanleyton6@gmail.com>
2024-07-06 12:04:52 -0400

    modified:   ProyectoCumbres/__pycache__/__init__.cpython-312.pyc 	modified:   ProyectoCumbres/__pycache__/settings.cpython-312.pyc 	modified:   ProyectoCumbres/__pycache__/urls.cpython-312.pyc 	modified:   ProyectoCumbres/__pycache__/wsgi.cpython-312.pyc 	modified:   SitioWeb/__pycache__/__init__.cpython-312.pyc 	modified:   SitioWeb/__pycache__/admin.cpython-312.pyc 	modified:   SitioWeb/__pycache__/apps.cpython-312.pyc 	modified:   SitioWeb/__pycache__/forms.cpython-312.pyc 	modified:   SitioWeb/__pycache__/models.cpython-312.pyc 	modified:   SitioWeb/__pycache__/urls.cpython-312.pyc 	modified:   SitioWeb/__pycache__/views.cpython-312.pyc 	modified:   SitioWeb/admin.py 	modified:   SitioWeb/forms.py 	modified:   SitioWeb/migrations/__pycache__/0001_initial.cpython-312.pyc 	modified:   SitioWeb/migrations/__pycache__/0002_delete_asistente_delete_imagen_delete_utp_and_more.cpython-312.pyc 	modified:   SitioWeb/migrations/__pycache__/__init__.cpython-312.pyc 	modified:   SitioWeb/models.py 	new file:   SitioWeb/templates/listarConsulta.html 	modified:   SitioWeb/templates/paginas/Contacto.html

--- SitioWeb/models.py
+++ SitioWeb/models.py
@@ -1,13 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Curso(models.Model):
+    nombreCurso = models.CharField(max_length=20)
+
+
 class consulta(models.Model):
     id_consulta = models.AutoField(db_column='idConsulta', primary_key=True)
     nombre_c = models.CharField(max_length = 20)
     apellido_c = models.CharField(max_length = 20)
     numero_c = models.IntegerField()
     correo_c = models.EmailField(max_length = 254)
+    curso = models.ForeignKey(Curso, on_delete=models.PROTECT)
     consulta_c = models.TextField()
+
+    def __str__(self):
+        return self.id_consulta
+
+    class Meta:
+        db_table = 'SitioWeb_consulta'
 
 class Profesor(models.Model):
     nombre_prof = models.CharField(max_length=20)
@@ -20,4 +32,5 @@ class Profesor(models.Model):
 
     class Meta:
         db_table = 'SitioWeb_profesor'
+
 
