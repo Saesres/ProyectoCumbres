ba586f1523dc3f1893b81a663f341053cb405991
aywey <yeanleyton6@gmail.com>
2024-07-08 09:28:19 -0400

    modified:   ProyectoCumbres/__pycache__/settings.cpython-312.pyc 	modified:   ProyectoCumbres/settings.py 	modified:   SitioWeb/templates/paginas/Contacto.html 	modified:   SitioWeb/templates/paginas/agregarProfesor.html 	modified:   SitioWeb/templates/paginas/estadoConsulta.html 	modified:   SitioWeb/templates/paginas/listarConsulta.html 	modified:   SitioWeb/templates/paginas/listarProfesor.html 	modified:   SitioWeb/templates/paginas/modificarProfesor.html 	modified:   db.sqlite3

--- ProyectoCumbres/settings.py
+++ ProyectoCumbres/settings.py
@@ -128,3 +128,12 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR,'ProyectoCumbres/static'),)
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_COOKIE_NAME = 'sessionid'
+SESSION_COOKIE_AGE = 1209600  
+SESSION_COOKIE_SECURE = False 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False  
+SESSION_SAVE_EVERY_REQUEST = True  
+
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
